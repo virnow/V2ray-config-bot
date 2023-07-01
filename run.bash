32f23f219f1df9a501edd958d74ae97982ef3556
sasan <74091294+virnow@users.noreply.github.com>
2023-07-01 12:50:19 +0330

    Update run.bash

--- run.bash
+++ run.bash
@@ -11,6 +11,7 @@ sleep 5
 nano V2ray-config-bot-main/config.py
 echo "Now Run Bot in background"
 sleep 5
-(python3 V2ray-config-bot-main/main.py &)
-(python3 V2ray-config-bot-main/update.py &)
+(cd V2ray-config-bot-main && python3 main.py &)
+(cd V2ray-config-bot-main && python3 update.py &)
+echo "End"
 exit

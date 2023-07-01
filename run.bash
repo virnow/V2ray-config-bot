d01919530ba73bbd957a70017fec76c83999b2e8
sasan <74091294+virnow@users.noreply.github.com>
2023-07-01 11:33:35 +0330

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 wget https://github.com/virnow/V2ray-config-bot/archive/refs/heads/main.zip
-sleep 10
 unzip main.zip
 cd V2ray-config-bot-main
-pip3 install -r requirements.txt
+pip3 install -r V2ray-config-bot-main/requirements.txt
 echo "Edit config File"
-nano config.py
+nano V2ray-config-bot-main/config.py
 echo "Now Run Bot in background"
-tmux new -d 'python3 main.py > main.log'
-tmux new -d 'python3 update.py > update.log'
+tmux new -d 'python3 V2ray-config-bot-main/main.py > main.log'
+tmux new -d 'python3 V2ray-config-bot-main/update.py > update.log'
 exit
